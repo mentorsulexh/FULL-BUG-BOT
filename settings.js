f2251f5001a2c236ce2ee0d96e6bed3eedb7d72f
mentorsulexh <mentorsulexh@gmail.com>
2025-03-20 09:12:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "254793006704"
+global.ownername = "🐛Sulexh Tech"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '254793006704'  //creator number
+global.ownername = '🐛Sulexh Tech' //owner name
+global.botname = 'Sulexh Tech ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
